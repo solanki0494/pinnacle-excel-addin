60984b274fcf95fbf4d085b05425427b694e9f65
Mohit Solanki <mohitsolanki@MBP-H9Y62R73XH.(none)>
2025-07-18 10:30:22 +0530

    test

--- src/commands/commands.ts
+++ src/commands/commands.ts
@@ -2,29 +2,6 @@
 
 import { copyOperatingExpenses, showNotification as serviceShowNotification } from '../services/operatingExpensesService';
 
-// Declare global types for Office.js
-declare global {
-  interface Window {
-    officeReady: boolean;
-  }
-}
-
-// Initialize Office.js when ready
-function initializeOffice() {
-  if (typeof Office !== 'undefined' && Office.onReady) {
-    Office.onReady(() => {
-      console.log('Office.js is ready for commands');
-      // Office is ready - commands will be available
-    });
-  } else {
-    console.log('Office.js not ready for commands, retrying...');
-    setTimeout(initializeOffice, 100);
-  }
-}
-
-// Start initialization
-initializeOffice();
-
 async function runCalculation(event: Office.AddinCommands.Event) {
   try {
     await Excel.run(async (context) => {
